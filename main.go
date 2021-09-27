5a102e007b97b5810ee0136c2068147b88be95ed
Teetawat <teetawatriya@gmail.com>
2021-09-28 00:07:22 +0700

    add http

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
 func main() {
-	app.Listen(":3334")
+	http.HandleFunc("/apix/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello World")
+	})
+
+	http.HandleFunc("/apix/greet/", func(w http.ResponseWriter, r *http.Request) {
+		name := r.URL.Path[len("/greet/"):]
+		fmt.Fprintf(w, "Hello %s\n", name)
+	})
+
+	http.ListenAndServe(":3334", nil)
 }
