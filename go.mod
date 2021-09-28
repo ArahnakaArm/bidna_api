fb102ede30785c10a1b5d77415a7fd5a053e6066
Teetawat <teetawatriya@gmail.com>
2021-09-28 18:20:08 +0700

    connect db

--- go.mod
+++ go.mod
@@ -16,8 +16,18 @@ require (
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/go-stack/stack v1.8.0 // indirect
 	github.com/gofiber/jwt/v2 v2.2.7 // indirect
 	github.com/golang-jwt/jwt/v4 v4.0.0 // indirect
+	github.com/golang/snappy v0.0.3 // indirect
 	github.com/jmoiron/sqlx v1.3.4 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
+	github.com/xdg-go/scram v1.0.2 // indirect
+	github.com/xdg-go/stringprep v1.0.2 // indirect
+	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
+	go.mongodb.org/mongo-driver v1.7.2 // indirect
 	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
+	golang.org/x/sync v0.0.0-20190911185100-cd5d95a43a6e // indirect
+	golang.org/x/text v0.3.6 // indirect
 )
--- main.go
+++ main.go
@@ -1,11 +1,93 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+const uri = "mongodb://superadmin:123456@51.79.184.185:27017/"
+
 func main() {
+
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		if err = client.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		}
+	}()
+
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+	/* 	// Ping the primary
+	   	if err := client.Ping(context.TODO(), readpref.Primary()); err != nil {
+	   		panic(err)
+	   	} */
+	fmt.Println("Successfully connected and pinged.")
+
+	db := client.Database("products")
+
+	collection := db.Collection("products")
+	/* 	filter := bson.M{"name": "test1"}
+	   	ctx, _ = context.WithTimeout(context.Background(), 5*time.Second)
+	   	cur ,err := collection.Find(ctx, bson.M{}).Decode(&result)
+	   	if err != nil {
+
+	   	}
+	*/
+	/* 	cursor, err := collection.Find(ctx, bson.M{})
+	   	if err != nil {
+	   		log.Fatal(err)
+	   	}
+	   	defer cursor.Close(ctx)
+	   	for cursor.Next(ctx) {
+	   		var episode bson.M
+	   		if err = cursor.Decode(&episode); err != nil {
+	   			log.Fatal(err)
+	   		}
+	   		fmt.Println(episode)
+	   	} */
+
+	var episodes []Result
+	cursor, err := collection.Find(ctx, bson.M{})
+	if err != nil {
+		panic(err)
+	}
+	if err = cursor.All(ctx, &episodes); err != nil {
+		panic(err.Error())
+	}
+
+	fmt.Println(episodes)
+
+	b, err := json.Marshal(episodes)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(string(b))
+	/* 	print(result.Name)
+	 */
+	/* findCursor, findErr := collection.Find(context.TODO(), bson.D{})
+	if findErr != nil {
+		panic(findErr)
+	}
+	var findResults []bson.M
+	if findErr = findCursor.All(context.TODO(), &findResults); findErr != nil {
+		panic(findErr)
+	}
+	for _, result := range findResults {
+		fmt.Println(result)
+	} */
+
 	http.HandleFunc("/apix/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello World")
 	})
@@ -14,6 +96,11 @@ func main() {
 		name := r.URL.Path[len("/greet/"):]
 		fmt.Fprintf(w, "Hello %s\n", name)
 	})
+	http.ListenAndServe("127.0.0.1:3000", nil)
+	/* http.ListenAndServe(":8000", nil) */
+}
 
-	http.ListenAndServe(":3334", nil)
+type Result struct {
+	Name   string   `bson:"name,omitempty"`
+	Colors []string `bson:"colors,omitempty"`
 }
